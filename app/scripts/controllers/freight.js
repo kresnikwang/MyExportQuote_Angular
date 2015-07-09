f011c7994dcac18a4c833994f320344810dd9a68
kresnikwang <kresnikwang@gmail.com>
2015-07-09 00:21:24 -0700

    freight

--- app/scripts/controllers/freight.js
+++ app/scripts/controllers/freight.js
@@ -9,5 +9,42 @@
  */
 angular.module('myexportquoteApp')
   .controller('FreightCtrl', function ($scope) {
-    $scope.awesomeThings = [    ];
+    $scope.airQuote = function(){
+      var airRate = document.getElementById("airRate").value;
+      var NoCS = document.getElementById("NoCS").value;
+      var NoPS = document.getElementById("NoPS").value;
+      var weightPerCase = document.getElementById("weightPerCase").value;
+      var trucking = document.getElementById("trucking").value;
+      var coolGuard = document.getElementById("coolGuard").value;
+      var otherPalletCharge = document.getElementById("otherPalletCharge").value;
+      var temperRecorder = document.getElementById("temperRecorder").value;
+      var documentFee = document.getElementById("documentFee").value;
+      var phytoFee = document.getElementById("phytoFee").value;
+      var otherCharge = document.getElementById("otherCharge").value;
+      var finalResult = document.getElementById("finalResult");
+      var totalCost;
+      var costPerCase;
+
+
+      if(document.getElementById("looseRate").checked) {
+        totalCost = airRate * weightPerCase * NoCS * NoPS + NoPS * trucking + NoPS * coolGuard + NoPS * otherPalletCharge + 1 * temperRecorder + 1 * documentFee + 1 * phytoFee + 1 * otherCharge;
+
+        // + temperRecorder + documentFee + phytoFee + otherCharge
+        //1 * temperRecorder + 1 * documentFee + 1 * phytoFee + 1 * otherCharge
+
+        if (NoCS > 0 && NoPS > 0) {
+          costPerCase = totalCost / (NoCS * NoPS);
+        }
+      }
+      else if(document.getElementById("LD7").checked){
+        totalCost = airRate;
+        alert(airRate);
+        if (NoCS > 0 && NoPS > 0) {
+          costPerCase = totalCost / (NoCS * NoPS);
+        }
+      }
+      finalResult.value = "Freight Cost/Case: $"+costPerCase.toFixed(2)+"\nTotal Cost: $"+ totalCost.toFixed(2);
+
+    };
+
   });
