2762d628bba1dfe231de2d9ed86e61e8ff654a25
kresnikwang <kresnikwang@gmail.com>
2015-07-10 15:16:16 -0700

    enter

--- app/scripts/controllers/freight.js
+++ app/scripts/controllers/freight.js
@@ -56,3 +56,29 @@ angular.module('myexportquoteApp')
     };
 
   });
+
+  
+  function goAction(url) {
+  if (url != '') {
+    quoteinformation.target = "_blank";//通过对目标的判定决定是否在新窗口打开网页
+  }
+  quoteinformation.action = url;//formname为当前form的name 多个form时 在此处获取form即可
+  quoteinformation.submit();//提交表单
+}
+
+
+function weight() {
+  var weightPerPallet = document.getElementById("weightPerPallet");
+  var NoCS = document.getElementById("NoCS");
+  var weightPerCase = document.getElementById("weightPerCase");
+
+  NoCS.addEventListener("input", function () {
+    weightPerPallet.value = (weightPerCase.value * NoCS.value).toFixed(2);
+  });
+  weightPerPallet.addEventListener("input", function () {
+    weightPerCase.value = (weightPerPallet.value / NoCS.value).toFixed(2);
+  });
+  weightPerCase.addEventListener("input", function () {
+    weightPerPallet.value = (weightPerCase.value * NoCS.value).toFixed(2);
+  });
+}
